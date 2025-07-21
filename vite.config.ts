0503b21be94587007ba2ce24442decb89f7a7798
Feliposooo <felipepciardella157@gmail.com>
2025-07-20 21:49:56 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/home-manager-condo/", // Necessário para GitHub Pages
+  base: "./", // Necessário para GitHub Pages
   server: {
     host: "::",
     port: 8080,
